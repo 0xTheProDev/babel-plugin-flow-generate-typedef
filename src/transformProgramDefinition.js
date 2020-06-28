2d30f77c58be1439b93fbda3ad7ef1b919c4aa1a
Progyan Bhattacharya <progyan@hackerrank.com>
2020-06-28 16:17:54 +0530

    [Feat] Optional Typed Function Param
    Signed-off-by: Progyan Bhattacharya <progyan@hackerrank.com>

--- src/transformProgramDefinition.js
+++ src/transformProgramDefinition.js
@@ -18,12 +18,14 @@ export default function transformProgramDefinition(t, node, state) {
     },
   } = state;
 
+  // Figure out module name from file absolute path and root path
   let moduleName = absolutePath.substring(root.length, absolutePath.length);
 
   if (moduleName.startsWith('/')) {
     moduleName = moduleName.substring(1);
   }
 
+  // Remove file extensions if any
   if (moduleName.endsWith('.js')) {
     moduleName = moduleName.substring(0, moduleName.length - 3);
   }
@@ -32,7 +34,9 @@ export default function transformProgramDefinition(t, node, state) {
     moduleName = moduleName.substring(0, moduleName.length - 4);
   }
 
+  // Create module declaration
   const moduleKind = sourceType === 'module' ? 'ES' : 'CommonJS';
   const moduleDeclaration = t.declareModule(t.identifier(`"${moduleName}"`), t.blockStatement(body), moduleKind);
+
   return t.program([ moduleDeclaration ], directives, sourceType, interpreter);
 }
